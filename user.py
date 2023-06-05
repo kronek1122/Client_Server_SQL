02547d4eb957b596b475b2da13cf3ef23df2fa63
Daniel <kronkowski.daniel@gmail.com>
2023-06-05 19:02:49 +0200

    modify class User - move db connection to __init__

--- user.py
+++ user.py
@@ -11,26 +11,26 @@ db_host = os.getenv('host')
 db_port = os.getenv('port')
 
 
-
 class User:
     '''Represents a user in the system with methods for user registration, login, 
     show all existing users, sending message, check inbox and chech only unread messages.'''
 
     def __init__(self):
         self.active_user = ''
+        self.db = DatabaseManager(db_database, db_user, db_password, db_host)
+
 
     def register(self, username, password, is_admin='false'):
         '''Adding a new user'''
 
-        db = DatabaseManager(db_database, db_user, db_password, db_host)
-        msg = db.add_user(username, password, is_admin)
+        msg = self.db.add_user(username, password, is_admin)
         return json.dumps(msg, indent=1)
+
 
     def login(self, username, password):
         '''Login user function'''
 
-        db = DatabaseManager(db_database, db_user, db_password, db_host)
-        msg, self.active_user = db.login_user(username, password)
+        msg, self.active_user = self.db.login_user(username, password)
         return json.dumps(msg, indent=1)
 
 
@@ -38,8 +38,7 @@ class User:
         '''return list of existing users'''
 
         if self.active_user != '':
-            db = DatabaseManager(db_database, db_user, db_password, db_host)
-            all_users = db.get_users()
+            all_users = self.db.get_users()
             return json.dumps(all_users)
         
         else:
@@ -49,18 +48,16 @@ class User:
     def send_message(self, username, message):
         '''sending message to other users'''
 
-        db = DatabaseManager(db_database, db_user, db_password, db_host)
-
         if not self.active_user:
             return json.dumps('Command available only for logged users', indent=1)
 
         if self.active_user == username:
             return json.dumps("You can't send message to yourself", indent=1)
 
-        if db.count_unread(username) >= 5 and not (db.is_user_admin(self.active_user) or db.is_user_admin(username)):
+        if self.db.count_unread(username) >= 5 and not (self.db.is_user_admin(self.active_user) or self.db.is_user_admin(username)):
             msg = f'Message could not be sent, mailbox user {username} is full'
         else:
-            msg = db.send_message(username, message, self.active_user)
+            msg = self.db.send_message(username, message, self.active_user)
         
         return json.dumps(msg, indent=1)
 
@@ -68,18 +65,16 @@ class User:
     def check_inbox(self, query):
         '''return messages in user inbox'''
 
-        db = DatabaseManager(db_database, db_user, db_password, db_host)
-
         if self.active_user != '':
-            if len(query)>1 and db.is_user_admin(self.active_user) is True:
-                msg = db.get_message(query[1])
+            if len(query)>1 and self.db.is_user_admin(self.active_user) is True:
+                msg = self.db.get_message(query[1])
                 
-            elif len(query)>1 and db.is_user_admin(self.active_user) is False:
+            elif len(query)>1 and self.db.is_user_admin(self.active_user) is False:
                 msg = 'You do not have permission to check another user mail'
 
             else:
-                msg = db.get_message(self.active_user)
-                db.change_from_unread(self.active_user)
+                msg = self.db.get_message(self.active_user)
+                self.db.change_from_unread(self.active_user)
             
         else: msg = 'First you must log in!'
         return json.dumps(msg, indent=1)
@@ -88,11 +83,10 @@ class User:
     def check_unread_messages(self):
         '''return only unread messages in user inbox'''
         
-        db = DatabaseManager(db_database, db_user, db_password, db_host)
         if self.active_user != '':
-            if db.is_msg_unread(self.active_user):
-                msg = db.get_unread_message(self.active_user)
-                db.change_from_unread(self.active_user)
+            if self.db.is_msg_unread(self.active_user):
+                msg = self.db.get_unread_message(self.active_user)
+                self.db.change_from_unread(self.active_user)
             else:
                 msg = "Your unread message inbox is empty"
         else:
